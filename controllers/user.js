3ba3735a0835e678026a7796f1652acdbe26f727
sathish-goud <sathish.panthangi13@gmail.com>
2023-10-23 00:09:18 +0530

    User - signin

--- controllers/user.js
+++ controllers/user.js
@@ -25,6 +25,51 @@ exports.signup = async (req, res, next)=>{
             message: error.message
         })
     }
+}
 
-    res.json({message: "Controller user is working"})
+
+exports.signin = async (req, res, next)=>{
+    
+    try{
+        const {email, password} =req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                success: true,
+                message: "Email and password are required"
+            })
+        }
+
+        //check user e-mail
+        const user = await User.findOne({email});
+        
+        if(!user){
+            return res.status(400).json({
+                success: true,
+                message: "Invalid Credentials"
+            })
+        }
+
+        // verify user password
+        const isMatched = await user.comparePassword(password);
+        
+        if (!isMatched) {
+            return res.status(400).json({
+                success: true,
+                message: "Invalid Credentials"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            user
+        })
+
+    }
+    catch(error){
+        console.log(error);
+        return res.status(400).json({
+            success: true,
+            message: "Can not log in, check your credentials"
+        })
+
+    }
 }
